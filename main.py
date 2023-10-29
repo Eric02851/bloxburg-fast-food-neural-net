7083302b58ac3a8cf63bfb70f2a2898b89ea967e
Eric Still <58347689+Eric02851@users.noreply.github.com>
2023-10-28 20:35:02 -0700

    Add valid order check and csv writing
    Fully working

--- main.py
+++ main.py
@@ -3,89 +3,125 @@ import numpy as np
 import cv2 as cv
 import time
 import os
-import pydirectinput
+from pynput import mouse
+import csv
+import threading
 
-centerPlus = (475, 1270) #For 2 and 4 items
-leftPlus = (476, 1160) #For 3 items
-plusColor = [100, 100, 100]
+burger4min = 865
+burger4max = 1691
 
-plusSideOffset = 205
-plusArmOsset = 22 #There should be no black in image on the sides
+burger3min = 965
+burger3max = 1587
 
-maxYOffset = 63
-minYOffset = 77
+burger2min = 1065
+burger2max = 1490
 
-y1 = 0
-y2 = 0
+windowMiddle = 415
+minY = 410
+maxY = 550
 
-drinkFries = [(412, 1199),(552, 1360)]
+csvHeader = ["imageName", "labels"]
+imageFolder = r"C:\Users\ziggy\Documents\GitHub\bloxburg-fast-food-neural-net\labeled"
+csvFolder = r"C:\Users\ziggy\Documents\GitHub\bloxburg-fast-food-neural-net\csv"
 
-unlabledFolder = r"C:\Users\ziggy\Documents\GitHub\bloxburg-fast-food-neural-net\unlabled"
+class MenuButton:
+    def __init__(self, minY, maxY, minX, maxX, name, subButtons = None):
+        self.minY = minY
+        self.maxY = maxY
+        self.minX = minX
+        self.maxX = maxX
+        self.name = name
+
+        self.subButtons = subButtons
+
+    def clicked(self, x, y):
+        if y >= self.minY and y <= self.maxY and x >= self.minX and x <= self.maxX:
+            return True
+        return False
+
+burgerOptions = [
+    MenuButton(423, 504, 2132, 2272, "onion"),
+    MenuButton(540, 585, 2130, 2265, "cheese"),
+    MenuButton(615, 695, 2069, 2185, "patty"),
+    MenuButton(596, 698, 2203, 2328, "vegiePatty"),
+    MenuButton(704, 810, 2128, 2265, "tomato"),
+    MenuButton(827, 911, 2132, 2269, "lettuce"),
+    MenuButton(323, 412, 2288, 2376, "undo")
+]
+
+sideOptions = [
+    MenuButton(425, 567, 2033, 2179, "fries"),
+    MenuButton(598, 740, 2033, 2179, "sticks"),
+    MenuButton(768, 911, 2033, 2179, "rings")
+]
+
+drinkOptions = [
+    MenuButton(425, 567, 2033, 2179, "soda"),
+    MenuButton(598, 740, 2033, 2179, "juice"),
+    MenuButton(768, 911, 2033, 2179, "shake")
+]
+
+sizeOptions = [
+    MenuButton(425, 567, 2218, 2364, "small"),
+    MenuButton(598, 740, 2218, 2364, "medium"),
+    MenuButton(768, 911, 2218, 2364, "large")
+]
+
+catagoryOptions = [
+    MenuButton(375, 475, 2411, 2509, "burger", burgerOptions),
+    MenuButton(538, 635, 2411, 2509, "side", sideOptions),
+    MenuButton(701, 797, 2411, 2509, "drink", drinkOptions),
+    MenuButton(861, 960, 2411, 2509, "done")
+]
+
+order = {
+    "done": False,
+    "valid": False,
+    "burger": [],
+    "side": {
+        "name": "",
+        "size": ""
+    },
+    "drink": {
+        "name": "",
+        "size": ""
+    }
+}
+
+sideOptions += sizeOptions
+drinkOptions += sizeOptions
+catagory = catagoryOptions[0]
+
+def padImage(screenshot, targetWidth):
+    padWidth = targetWidth - screenshot.shape[1]
+
+    padLeft = padWidth // 2
+    padRight = padWidth - padLeft
+
+    return np.pad(screenshot, ((0, 0), (padLeft, padRight), (0, 0)), mode='constant', constant_values=255)
 
 def displayImg(screenshot):
-    #screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR)
     cv.imshow('screenshot', screenshot)
     cv.waitKey()
     cv.destroyAllWindows()
 
-def getMiddle(screenshot, plusCords):
-        middle = False
-        j = 0
-        for j in range(20):
-            if np.allclose(screenshot[plusCords[0], plusCords[1] + j], plusColor, atol=10):
-                for k in range(18):
-                    if np.allclose(screenshot[plusCords[0] + k, plusCords[1] + j - 1], plusColor, atol=10):
-                        middle = [plusCords[0] + k, plusCords[1] + j]
-                        return middle
-
-def getLeftItem(screenshot, middle, itemOffset):
-    x1 = middle[1] - plusSideOffset * (itemOffset + 1) + plusArmOsset
-    x2 = middle[1] - plusSideOffset * itemOffset - plusArmOsset
-    return screenshot[y1:y2, x1:x2]
-
-def getRightItem(screenshot, middle, itemOffset):
-    x1 = middle[1] + plusSideOffset * itemOffset + plusArmOsset
-    x2 = middle[1] + plusSideOffset * (itemOffset + 1) - plusArmOsset
-    return screenshot[y1:y2, x1:x2]
-
 def getBurger(screenshot):
-    global y1, y2
+    if np.array_equal(screenshot[windowMiddle, burger4min], [255, 255, 255]):
+        screenshot = screenshot[minY:maxY, burger4min:burger4max]
+        print("burger4")
+        return padImage(screenshot, 826)
 
-    itemCount = 0
-    middle = getMiddle(screenshot, centerPlus)
-    if middle:
-        plusToLeft = np.allclose(screenshot[middle[0], middle[1] + plusSideOffset], plusColor, atol=10)
-        itemCount = 4 if plusToLeft else 2
-    else:
-        middle = getMiddle(screenshot, leftPlus)
-        itemCount = 3
+    if np.array_equal(screenshot[windowMiddle, burger3min], [255, 255, 255]):
+        screenshot = screenshot[minY:maxY, burger3min:burger3max]
+        print("burger3")
+        return padImage(screenshot, 826)
 
-    if not middle:
-        return []
-
-    y1 = middle[0] - minYOffset
-    y2 = middle[0] + maxYOffset
-
-    items = []
-    if itemCount == 4:
-        items.append(getLeftItem(screenshot, middle, 1))
-        items.append(getLeftItem(screenshot, middle, 0))
-        items.append(getRightItem(screenshot, middle, 0))
-        items.append(getRightItem(screenshot, middle, 1))
-
-    elif itemCount == 3:
-        items.append(getLeftItem(screenshot, middle, 0))
-        items.append(getRightItem(screenshot, middle, 0))
-        items.append(getRightItem(screenshot, middle, 1))
-
-    else:
-        items.append(getLeftItem(screenshot, middle, 0))
-        items.append(getRightItem(screenshot, middle, 0))
-
-    return items
+    screenshot = screenshot[minY:maxY, burger2min:burger2max]
+    print("burger2/1")
+    return padImage(screenshot, 826)
 
 def getSideAndDrink(screenshot):
-    return screenshot[drinkFries[0][0]:drinkFries[1][0], drinkFries[0][1]:drinkFries[1][1]]
+    return screenshot[minY:maxY, burger2min:burger2max]
 
 def takeScreenshot():
     screenshot = pyautogui.screenshot()
@@ -95,78 +131,151 @@ def takeScreenshot():
     return screenshot
 
 def getOrder():
-    screenshot = takeScreenshot()
-
     print("Waiting for customer")
+    screenshot = takeScreenshot()
     while not np.array_equal(screenshot[320, 1412], [255, 255, 255]):
         time.sleep(0.1)
         screenshot = takeScreenshot()
 
     time.sleep(0.5)
-    items = getBurger(screenshot)
-    print(f"Burger: {len(items)}")
-    if len(items) == 0:
-        print("Burger failed")
-
-        time.sleep(1)
-        pydirectinput.press('space')
-        time.sleep(2)
-        pydirectinput.press('e')
-        return getOrder()
+    burger = getBurger(screenshot)
+    print("Got burger")
 
     time.sleep(5)
     screenshot = takeScreenshot()
-
     side = getSideAndDrink(screenshot)
-    print("Side")
+    print("Got side")
 
     time.sleep(2.5)
     screenshot = takeScreenshot()
-
     drink =  []
     if np.array_equal(screenshot[320, 1412], [255, 255, 255]):
-        print("Drink")
+        print("Got drink")
         drink = getSideAndDrink(screenshot)
         time.sleep(3)
 
-    print("Done", end="\n\n")
-    return items, side, drink
+    return {"burger": burger, "side": side, "drink": drink}
 
-def getImageId(subfolder):
-    imageList = os.listdir(f"{unlabledFolder}/{subfolder}")
-    imageList = sorted(imageList, key=lambda x: int(x.split('.')[0]))
+def onClick(x, y, mouseButton, pressed):
+    global catagory
+    global order
 
-    id = 0
-    if len(imageList) != 0:
-        id = int(imageList[-1].split('.')[0]) + 1
+    if pressed and mouseButton == mouseButton.left:
+        #print(f"Mouse clicked at ({x}, {y}) with button {mouseButton}")
+        for b in catagory.subButtons:
+            if b.clicked(x, y):
+                if catagory.name == "burger":
+                    if b.name == "undo":
+                        if order[catagory.name]: order[catagory.name].pop()
+                    else:
+                        order[catagory.name].append(b.name)
+                else:
+                    if b.name != "small" and b.name != "medium" and b.name != "large":
+                        if b.name == order[catagory.name]["name"]:
+                            order[catagory.name]["name"] = ""
+                            order[catagory.name]["size"] = ""
+                        else:
+                            order[catagory.name]["name"] = b.name
+                    else:
+                        if order[catagory.name]["name"]:
+                            order[catagory.name]["size"] = b.name[0].upper()
 
-    return id
+                print(order)
+                break
+
+        for b in catagoryOptions:
+            if b.clicked(x, y):
+                if b.name == "done": #check here if the order is good or not
+                    time.sleep(1.5)
+                    screenshot = takeScreenshot()
+                    if np.array_equal(screenshot[640, 2250], [74, 175, 101]):
+                        print("valid")
+                        order["valid"] = True
+                    order["done"] = True
+
+                    print(order)
+                    print("Done")
+                    catagory = catagoryOptions[0]
+                else: catagory = b
+                break
+
+def getImageIds():
+    imageIds = {"burger": 0, "side": 0, "drink": 0}
+
+    for i in imageIds:
+        imageList = os.listdir(f"{imageFolder}/{i}")
+        imageList = sorted(imageList, key=lambda x: int(x.split('.')[0][5:]))
+
+        if len(imageList) != 0:
+            imageIds[i] = int(imageList[-1].split('.')[0][5:]) + 1
+
+    return imageIds
+
+def writeImages(images, imageIds):
+    for i in images:
+        if not len(images[i]): continue
+        imagePath = f"{imageFolder}/{i}/image{imageIds[i]}.png"
+        cv.imwrite(imagePath, images[i])
+        imageIds[i] += 1
+
+    return imageIds
+
+def writeCSVs(images, imageIds, order):
+    for i in images:
+        row = [f"image{imageIds[i]}.png", ""]
+        if i == "burger":
+            j = 0
+            items = order["burger"]
+
+            while j < len(items) - 1:
+                if items[j] == items[j + 1]:
+                    row[1] += f",{items[j]}2"
+                    j += 2
+                else:
+                    row[1] += f",{items[j]}"
+                    j += 1
+
+            if j == len(items) - 1:
+                row[1] += f",{items[j]}"
+
+            row[1] = row[1][1:]
+        else:
+            if not len(images[i]): continue
+            row[1] = order[i]["name"] + order[i]["size"]
+
+        csvPath = f"{csvFolder}/{i}.csv"
+        with open(csvPath, 'a', newline='') as csvFile:
+            writer = csv.writer(csvFile)
+            writer.writerow(row)
+
+def listenerThread():
+    with mouse.Listener(on_click=onClick) as listener:
+        listener.join()
+
 
 def main():
-    burgerId = getImageId("burgers")
-    sideId = getImageId("sides")
-    drinkId = getImageId("drinks")
-    print(burgerId, sideId, drinkId)
+    imageIds = getImageIds()
+    print(imageIds["burger"], imageIds["side"], imageIds["drink"])
+
+    threading.Thread(target=listenerThread).start()
 
     while True:
-        burgerItems, side, drink = getOrder()
+        images = getOrder()
 
-        for i in range(len(burgerItems)):
-            imagePath = f"{unlabledFolder}/burgers/{burgerId}.png"
-            cv.imwrite(imagePath, burgerItems[i])
-            burgerId += 1
+        while not order["done"]:
+            time.sleep(0.5)
 
-        imagePath = f"{unlabledFolder}/sides/{sideId}.png"
-        cv.imwrite(imagePath, side)
-        sideId += 1
+        if order["valid"]:
+            imageIds = writeImages(images, imageIds)
+            writeCSVs(images, imageIds, order)
 
-        if len(drink):
-            imagePath = f"{unlabledFolder}/drinks/{drinkId}.png"
-            cv.imwrite(imagePath, drink)
-            drinkId += 1
+        order["done"] = False
+        order["valid"] = False
+        order["burger"].clear()
 
-        pydirectinput.press('space')
+        for i in order["side"]: order["side"][i] = ""
+        for i in order["drink"]: order["drink"][i] = ""
+
         time.sleep(2)
-        pydirectinput.press('e')
 
 main()
